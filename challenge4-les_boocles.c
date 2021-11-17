4cc7146c0cd785fe3494e031b693ae2764c90cbd
devtaha <tbouhdach2000@gmail.com>
2021-11-17 16:56:18 +0100

    challenge4-les_boocles

---
+++ challenge4-les_boocles.c
@@ -0,0 +1,18 @@
+int main()
+{
+   int NbrInverse = 0, nbr;
+ 
+   printf("Entrez un nombre à inverser\n");
+   scanf("%d", &nbr);
+ 
+   while (nbr > 0)
+   {
+      NbrInverse = NbrInverse * 10;
+      NbrInverse = NbrInverse + nbr%10;
+      nbr = nbr/10;
+   }
+ 
+   printf("Le nombre en sens inverse est = %d", NbrInverse);
+ 
+   return 0;
+}
